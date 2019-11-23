7b018692622325022c467d048871cb21e94afd18
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-23 22:14:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,7 @@ next_color = "violet"
 colors_in_the_rainbow.push(next_color)
 end
 
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+@new_neighborhood = "Staten Island
+bouroughs_in_nyc.unshift(@new_neighborhood)
+end
