866449b42918a2af304dc71f65dff86503f88fe9
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-23 22:50:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,5 +31,7 @@ end
   
     @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
     @another_language = "Python"
-    @new_array = using_insert(@list_of_programming_languages, @another_language)
+    @list_of_programming_languages.insert(@another_language)[4]
   end
+  
+  
